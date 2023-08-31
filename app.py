223fb22fd8a7444c2ddcbb37a0f1f22e878d6d03
Sabesh47 <sabeshkrishnan66@gmail.com>
2023-09-01 00:18:14 +0530

    Register Page
    Password verification and unique usernames

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
-
-@app.route("/register")
+Existing_unames=['asd','sed','bob','Hawlk']
+@app.route("/register",methods=('GET','POST'))
 def default():
+    if request.method =='Post':
+        uname=request.form.get('name')
+        pwd=request.form.get('pwd')
+        cpwd=request.form.get('cpwd')
+        if uname in Existing_unames:
+            print('Username is already taken')
+        elif uname not in Existing_unames:
+            Existing_unames.append(uname)
+        elif pwd==cpwd:
+            print('Registered successfully')
+        elif pwd != cpwd:
+            print('Passwords do not match....enter again')
+        
     return render_template("hello.html")
-
