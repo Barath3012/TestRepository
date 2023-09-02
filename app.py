eab9129c3830ba607e12ca0930d4280166d1e94d
Sabesh47 <sabeshkrishnan66@gmail.com>
2023-09-02 22:52:42 +0530

    registration V2

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask,render_template,request
 
 app = Flask(__name__)
-Existing_unames=['asd','sed','bob','Hawlk']
+db={'asd':'123','sed':'234','bob':'345','Hawlk':'456'}
 @app.route("/register",methods=('GET','POST'))
 def default():
-    if request.method =='Post':
+    if request.method =='POST':
         uname=request.form.get('name')
         pwd=request.form.get('pwd')
         cpwd=request.form.get('cpwd')
-        if uname in Existing_unames:
-            print('Username is already taken')
-        elif uname not in Existing_unames:
-            Existing_unames.append(uname)
-        elif pwd==cpwd:
-            print('Registered successfully')
+        if pwd==cpwd:
+            if uname in db:
+                print('Username is already taken')
+            elif uname not in db:
+                db[uname]=pwd
+                print('Registered successfully')
+                print(db)
         elif pwd != cpwd:
             print('Passwords do not match....enter again')
+        
+        
         
     return render_template("hello.html")
