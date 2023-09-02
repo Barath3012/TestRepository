1964f8e8f215407c19a55a3d8ae2041c3f7e158d
sathish0211 <138810359+sathish0211@users.noreply.github.com>
2023-09-02 22:50:44 +0530

    added login page

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
-star={"sathish":12345,"ragul":ragul@123,"sanjay":sanjaymani}
-@app.route("/login")
+db={"sathish":"12345","ragul":"ragul@123","sanjay":"sanjaymani"}
+@app.route("/register",methods=('GET','POST'))
 def cric():
+    if request.method =='POST':
+        username=request.form.get('name')
+        password=request.form.get('password')
+        if username in db:
+            if password == db[username]:
+                print("login successful")
+            else:
+                print("login unsuccessful")
+        else:
+            print("login unsuccessful")
     return render_template('hello.html')
