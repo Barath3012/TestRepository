5e35914ed736bafa616ea70ddbcef7d460b8ef03
Sabesh47 <sabeshkrishnan66@gmail.com>
2023-09-13 20:13:30 +0530

    Login page
    Hyper link between Login and Register pages are added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 db={'asd':'123','sed':'234','bob':'345','Hawlk':'456'}
-@app.route("/register",methods=('GET','POST'))
+@app.route("/Register",methods=('GET','POST'))
 def default():
     if request.method =='POST':
         uname=request.form.get('name')
@@ -18,6 +18,16 @@ def default():
         elif pwd != cpwd:
             print('Passwords do not match....enter again')
         
-        
-        
     return render_template("hello.html")
+@app.route("/Login",methods=('GET','POST'))
+@app.route("/",methods=('GET','POST'))
+def default1():
+    if request.method =='POST':
+        uname=request.form.get('name')
+        pwd=request.form.get('pwd')
+        if uname in db:
+            print('Username is taken')
+        elif uname=='' or pwd=='':
+            print('Empty password or username')
+        
+    return render_template("Login.html")
