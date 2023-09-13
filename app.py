ef599d8fff5397810999f71496a9352d0be4ed05
boop22 <66866805+Barath3012@users.noreply.github.com>
2023-09-13 21:45:45 +0530

    Database and Jinja

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, flash
 from database import database
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route("/")
+params = {
+    "var1":"sabesh",
+    "var2":"barath",
+    "var3": {
+        "barath":1234
+             }
+          }
+
+@app.route("/",methods=["GET","POST"])
 def default():
-    return render_template("hello.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        flash("Welcome, "+name)
+
+    return render_template("hello.html",params=params)
